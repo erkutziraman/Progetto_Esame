8354a5807f68cac41db7d121688c98c188c8f7d7
ERKUT ZIRAMAN <64338238+erkutziraman@users.noreply.github.com>
2020-06-02 14:44:02 +0200

    Preparazione superclasse ConnessioneAPI

--- Conn_Server/ConnessioneSearch.java
+++ Conn_Server/ConnessioneSearch.java
@@ -1,27 +1,19 @@
 /*package Conn_Server;
 
-import Preleva_Ids.GetId;
-
-public class ConnessioneSearch {
-    String ricerche_tweet[] = { "Italy", "France", "Belgium", "Germany", "Austria", "Hungary", "Poland", "Russia",
-            "America", "Sweden", "Finland", "Turkey", "Cameroon", "Greece", "Spain", "Portugal", "England", "Iceland",
+/**
+ * ConnessioneSearch
+ */
+/*public class ConnessioneSearch extends ConnessioneAPI{
+    String url_ricerche="";
+    String ricerche_tweet[] = { "Love", "Life","Sunrise", "Food", "Restaurant", "Discovery", "News", "tbt",
+            "univpm", "France", "Italy", "Holland", "Tourism", "Travel", "Rome", "Portugal", "England", "Iceland",
             "China", "Poland" };
-    public void ids() {
-    String s="0";
-    ConnessioneAPI connessione= new ConnessioneAPI();
-    GetId ids= new GetId();
-    int i = 0;
-    do{
-    String url_ricerche = connessione.getUrl()+ "?q=%23" +ricerche_tweet[i]+ "&count=100";
-    connessione.setUrl(url_ricerche);
-    s=connessione.dati_api();
-    i++;
-    }while(s!="0"&&i<20);
-    if(s=="0"){
-        LeggifileJSON json=new LeggifileJSON();
-        s=json.data_file();
-    }
-    ids.Tweet_id(s);
-    }
+    String s="";
+
+    for ( i = 0; i < 20 && valido!=true; i++) {
+    url_ricerche = url  + "?q=%23" + ricerche_tweet[i] + "&count=100";
+    s=dati_api();
+        }
+     if(i==20) s="["+s+"]";
 }
 */
