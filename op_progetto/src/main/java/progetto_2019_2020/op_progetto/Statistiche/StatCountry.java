ccf3cca15931f0cf5c01a1e9aa013aef22f439d0
toyemryan <toyemryan86@gmail.com>
2020-06-12 05:27:47 +0200

    piu resoluzione del problema offline e del percors

--- op_progetto/src/main/java/progetto_2019_2020/op_progetto/Statistiche/StatCountry.java
+++ op_progetto/src/main/java/progetto_2019_2020/op_progetto/Statistiche/StatCountry.java
@@ -4,26 +4,26 @@ import java.util.Vector;
 import progetto_2019_2020.op_progetto.Preleva_Informazioni.TweetMetadata;
 
 public class StatCountry {
-    
+
     int b;
     String c = "";
- 
-     public void get_StatFiltriCountry(String a,Vector<TweetMetadata> s){
-     
-     int t = 0 ;
-     for(int i=0;i<s.size();i++){
-        if(s.get(i).getCountry().equals(a)){
-                 t++;
-               this.b=t;
-             }
-         }
-     }
- 
-     public Statmain risultato(){
-         String description = "Questa statistica ci restituisce il numero di tweet che sono stati postati nel paese selezionato" ;
-         int risultato = b;
-         Statmain statmain = new Statmain(description, risultato);
-         return statmain;
-     }
+
+    public void get_StatFiltriCountry(String a, Vector<TweetMetadata> s) {
+
+        int t = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s.get(i).getCountry().equals(a)) {
+                t++;
+                this.b = t;
+            }
+        }
+    }
+
+    public Statmain risultato() {
+        String description = "Questa statistica ci restituisce il numero di tweet che sono stati postati nel paese selezionato";
+        int risultato = b;
+        Statmain statmain = new Statmain(description, risultato);
+        return statmain;
+    }
 
 }
